ee906c2d29743b700ba8ef823cfc5261d9018f5d
alaaahmed149 <anewalaahehe@gmail.com>
2025-03-19 02:00:53 +0200

    SignOut

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { roboto } from "../fonts";
-import { usePathname } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
 import clsx from "clsx";
 import { useAuth } from "../features/auth/hooks";
 
@@ -59,9 +59,12 @@ const Menu = () => {
         </Link>
       ))}
       <span className="opacity-50 font-normal">Other</span>
-      <button onClick={() => signOut()} className="flex items-center gap-4 p-2 cursor-pointer hover:bg-gray-100 rounded-md">
-          <Image src="/log-out.svg" alt="" width={30} height={30}></Image>
-          <span className="hidden md:hidden lg:block">Sign Out</span>
+      <button
+        onClick={() => signOut(redirect("/login"))}
+        className="flex items-center gap-4 p-2 cursor-pointer hover:bg-gray-100 rounded-md"
+      >
+        <Image src="/log-out.svg" alt="" width={30} height={30}></Image>
+        <span className="hidden md:hidden lg:block">Sign Out</span>
       </button>
     </div>
   );
